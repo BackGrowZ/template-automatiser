e285281159228d6755999967d310a7e6d931315f
BackGrowZ <anthony.carreta@gmail.com>
2020-07-09 15:22:30 +0200

    Create Generator of Input

--- src/component/Editeur/Editeur.js
+++ src/component/Editeur/Editeur.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ChromePicker } from 'react-color'
+import { v4 as uuidv4 } from 'uuid'; // pour les keys
 import './edit.css'
 
 export default class Editeur extends Component {
@@ -7,45 +8,94 @@ export default class Editeur extends Component {
         super(props)
         this.state = {
             /* Config */
+            noscroll: false,
             pathname: null,
             hash: null,
             id: null,
+            initInput: true,
+
             /* Items */
             itemsActualPosition: null,
             itemsName: '',
             itemsLink: '',
             itemsPosition: [],
+
             /* Couleur */
             pickerColor: '',
+
             /* Reseau */
             reseauLink: '',
+
             /* Copyright */
             copyright: '',
             copyrightLink: '',
 
+            /* BoxSetting */
+            boxButtonFooter: [
+                ['Annuler', true, 'CancelBoxEdition', undefined], // label, afficher, ClassName, function(initialiser dans initFunction)
+                ['Valider', true, 'SubmitBoxEdition', undefined],
+                ['Supprimer', true, 'DeletBoxEdition', undefined]
+            ],
+            formState: {
+                itemsName: false,
+                itemsLink: false,
+                reseauLink: false,
+                copyright: false,
+                copyrightLink: false
+            },
+            form: {
+                itemsName: '',
+                itemsLink: '',
+                reseauLink: '',
+                copyright: '',
+                copyrightLink: '',
+            },
+            label: {
+                itemsName: 'Label',
+                itemsLink: 'URL',
+                reseauLink: 'URL',
+                copyright: 'Label',
+                copyrightLink: 'URL',
+            },
+            boxInput: [
+                [true, null, 'boxForm', '', 'itemsName', 'Nom'], // afficher, placeholder, ClassName, value, name, label
+                [true, null, 'boxForm', '', 'itemsLink', 'Lien'],
+                [true, null, 'boxForm', '', 'reseauLink', 'Lien'],
+                [true, null, 'boxForm', '', 'copyright', 'Nom'],
+                [true, null, 'boxForm', '', 'copyrightLink', 'Lien'],
+            ]
 
         }
         this.setItems = this.setItems.bind(this)
-        this.handleInputChange = this.handleInputChange.bind(this)
         this.init = this.init.bind(this)
         this.componentDidMount = this.componentDidMount.bind(this)
+        this.onFormInputChange = this.onFormInputChange.bind(this)
     }
     componentDidMount() {
-        window.addEventListener('scroll', this.noScroll(true));
+        window.addEventListener('scroll', this.noScroll(!this.state.noscroll));
         this.setState({
             pathname: window.location.pathname,
             hash: window.location.hash,
             id: window.location.hash.slice(4)
         })
+        
         setTimeout(() => this.init(), 100)
+        setTimeout(() => this.initFunction(), 500)
     }
 
     init() {
         if (this.state.hash !== null) {
+            const { form } = this.state
+            const { formState } = this.state
             switch (this.state.pathname) {
                 case '/items/':
                     this.setState({
-                        itemsActualPosition: `${this.state.hash.slice(4)} (actuelle)`
+                        itemsActualPosition: `${this.state.hash.slice(4)} (actuelle)`,
+                        formState: {
+                            ...formState,
+                            itemsName: true,
+                            itemsLink: true
+                        }, 
                     })
                     setTimeout(() => this.setItems(), 100)
                     break;
@@ -53,21 +103,43 @@ export default class Editeur extends Component {
                 case '/color/':
                     let color = this.props.color[this.state.id][1]
                     this.setState({
-                        pickerColor: color
+                        pickerColor: color,
+                        // formState: [false, false, false, false, false], // itemsName, itemsLink,reseauLink,copyright,copyrightLink
                     })
                     break;
 
                 case '/reseau/':
                     let reseau = this.props.reseau[this.state.id][2]
                     this.setState({
-                        reseauLink: reseau
+                        form: {
+                            ...form,
+                            reseauLink: reseau
+                        },
+                        formState: {
+                            ...formState,
+                            reseauLink: true,
+                        },
+                        reseauLink: reseau,
+                        // formState: [false, false, true, false, false], // itemsName, itemsLink,reseauLink,copyright,copyrightLink
                     })
                     break;
 
                 case '/copyright/':
                     let copyright = this.props.copyright[0]
                     let copyrightLink = this.props.copyright[1]
+
                     this.setState({
+                        form: {
+                            ...form,
+                            copyright: copyright,
+                            copyrightLink: copyrightLink
+                        },
+                        formState: {
+                            ...formState,
+                            copyright: true,
+                            copyrightLink: true
+                        },
+                        // formState: [false, false, false, true, true], // itemsName, itemsLink,reseauLink,copyright,copyrightLink
                         copyrightLink: copyrightLink,
                         copyright: copyright
                     })
@@ -81,6 +153,7 @@ export default class Editeur extends Component {
     }
 
     setItems() {
+        const { form } = this.state
         if (this.state.id) {
             let id = this.state.id
             const name = this.props.items[id][0]
@@ -94,6 +167,12 @@ export default class Editeur extends Component {
                 }
             }
             this.setState({
+                form: {
+                    ...form,
+                    itemsName: name,
+                    itemsLink: link,
+                },
+
                 itemsName: name,
                 itemsLink: link,
                 itemsPosition: position,
@@ -151,13 +230,38 @@ export default class Editeur extends Component {
             document.getElementsByTagName('body')[0].style.overflow = ''
         }
     }
-    handleInputChange(e) {
-        const target = e.target;
-        const value = target.value;
-        const name = target.id;
-        this.setState({ [name]: value });
+    onFormInputChange({ target }) {
+        let { form } = this.state
+        this.setState({
+            form: {
+                ...form,
+                [target.name]: target.value
+            }
+        })
+    }
+    initFunction() { // place les fonctions dans le state
+        const state = this.state
+        /* state */
+        const noScrollState = state.noscroll
+        let boxButtonFooterState = state.boxButtonFooter
+        /* Function */
+        const noScroll = () => this.noScroll(noScrollState)
+
+        boxButtonFooterState[0][3] = noScroll // button annuler
+        this.setState({
+            boxButtonFooter: boxButtonFooterState,
+        })
     }
 
+    boxButton(array) { // generateur de button
+        let button = array.map(result => // result[0] = label | result[1] = state | result[2] = ClassName | result[3] = function OnClick 
+            (result[1]) ?
+                <button key={uuidv4()} className={result[2]} onClick={result[3]} > {result[0]} </button>
+                :
+                null
+        )
+        return button
+    }
     render() {
         const handleColorChange = ({ hex }) => this.setState({ pickerColor: hex })
         let positionSelect = this.state.itemsPosition.map(position =>
@@ -176,14 +280,12 @@ export default class Editeur extends Component {
                 </div>
             ) : null
 
-        const items = (this.state.pathname === '/items/') ?
+        const items = (this.state.pathname === '/items/' && this.state.itemsActualPosition && this.state.boxInput[0][4]) ?
             (
                 <div>
                     <select value={this.state.itemsActualPosition} id='id' onChange={this.handleInputChange}>
                         {positionSelect}
                     </select>
-                    <input id='itemsName' onChange={this.handleInputChange} value={this.state.itemsName} />
-                    <input id='itemsLink' onChange={this.handleInputChange} value={this.state.itemsLink} />
                     <button onClick={() => this.updateItems()}>Validé</button>
                 </div>
             ) : null
@@ -205,20 +307,49 @@ export default class Editeur extends Component {
                     <button onClick={() => this.updateCopyright()}>Validé</button>
                 </div>
             ) : null
+        const boxButtonFooter = this.boxButton(this.state.boxButtonFooter)
 
+        let input = Object.entries(this.state.form).map(([key, value]) => {
+            return (
+                <input
+                    key={`input${key}`}
+                    name={key}
+                    placeholder={key}
+                    value={value}
+                    className='boxForm'
+                    onChange={this.onFormInputChange}
+                />
+            );
+        })
+        let label = Object.entries(this.state.label).map(([key, value]) => {
+            return (
+                <label
+                    key={`label${key}`}
+                    htmlFor={key}
+                    className='boxLabelInput'
+                >
+                    {value}
+                </label>
+            );
+        })
+        const formStateArray = Object.entries(this.state.formState)
         const boxTemplate =
             <div className='BoxEdition'>
                 <div className='HeaderBoxEdition'>
-                    <div className='CrossBoxEdition'><i onClick={() => this.noScroll(false)} className="fas fa-times" /></div>
+                    <div className='CrossBoxEdition'><i onClick={this.state.boxButtonFooter[0][3]} className="fas fa-times" /></div>
                     <div className='TitleBoxEdition'>TITlE OF BOX</div>
                 </div>
                 <div className='BodyBoxEdition'>
-                    Le contenu de ma box
+                    {input.map((reslut, key) => (formStateArray[key][1]) ?
+                        <div key={key} className='boxContainerInput'>
+                            {label[key]}
+                            {reslut}
+                        </div>
+                        : null
+                    )}
                 </div>
                 <div className='FooterBoxEdition'>
-                    <button className='CancelBoxEdition'>Annuler</button>
-                    <button className='SubmitBoxEdition'>Validé</button>
-                    <button className='DeletBoxEdition'>Supprimer</button>
+                    {boxButtonFooter}
                 </div>
 
             </div>
